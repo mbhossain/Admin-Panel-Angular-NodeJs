d2609787b5416414b54b9f97f99ff198ba0a3d78
mbhossain <king.cseiu@gmail.com>
2023-01-19 12:32:10 +0600

    password reset API created

--- admin-panel-service/controllers/auth-controller.js
+++ admin-panel-service/controllers/auth-controller.js
@@ -1,8 +1,12 @@
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import Joi from "joi";
+import crypto from "crypto";
 
 import User from "../models/User.js";
 import { createError } from "../utils/error.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import Token from "../models/token.js";
 
 
 export const register = async (req, res, next) => {
@@ -61,4 +65,61 @@ export const login = async (req, res, next) => {
         next(err)
     }
 
+}
+
+export const passwordReset = async (req, res) => {
+    try {
+        const schema = Joi.object({ email: Joi.string().email().required() });
+        const { error } = schema.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const user = await User.findOne({ email: req.body.email });
+        if (!user)
+            return res.status(400).send("user with given email doesn't exist");
+
+        let token = await Token.findOne({ userId: user._id });
+        console.log('token:', token)
+        console.log('!token:', !token)
+        if (!token) {
+            token = await new Token({
+                userId: user._id,
+                token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+        }
+
+        const link = `${process.env.BASE_URL}/password-reset/${user._id}/${token.token}`;
+        console.log('link:', link)
+        await sendEmail(user.email, "Password reset", link);
+
+        res.send("password reset link sent to your email account");
+    } catch (error) {
+        res.send("An error occured");
+        console.log(error);
+    }
+}
+
+export const passwordUpdated = async (req, res) => {
+    try {
+        const schema = Joi.object({ password: Joi.string().required() });
+        const { error } = schema.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const user = await User.findById(req.params.userId);
+        if (!user) return res.status(400).send("invalid link or expired");
+
+        const token = await Token.findOne({
+            userId: user._id,
+            token: req.params.token,
+        });
+        if (!token) return res.status(400).send("Invalid link or expired");
+
+        user.password = req.body.password;
+        await user.save();
+        await token.delete();
+
+        res.send("password reset sucessfully.");
+    } catch (error) {
+        res.send("An error occured");
+        console.log(error);
+    }
 }
--- admin-panel-service/routes/auth-route.js
+++ admin-panel-service/routes/auth-route.js
@@ -4,11 +4,16 @@ import express from "express";
 import {
     register
     , login
+    , passwordReset
+    , passwordUpdated
 } from '../controllers/auth-controller.js'
 
 const router = express.Router();
 
 router.post('/register', register);
-router.post('/login', login)
+router.post('/login', login);
+
+router.post('/password-reset', passwordReset);
+router.post('/password-reset/:userId/:token', passwordUpdated);
 
 export default router
